be166b62a7f29d3ce9c81b62cb4b8e60661058fb
orlando1080 <lewislsewell@gmail.com>
2023-04-14 22:53:56 +0100

    working multiple user authenitaction

--- spotify_visual.py
+++ spotify_visual.py
@@ -1,10 +1,12 @@
 from collections import defaultdict
 import configparser
+import json
+import requests
 
 import matplotlib.pyplot as plt
+
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-
 
 config = configparser.ConfigParser()
 config.read('config.cfg')
@@ -13,17 +15,48 @@ cid = config.get('SPOTIFY', 'CLIENT_ID')
 secret = config.get('SPOTIFY', 'CLIENT_SECRET')
 scope = 'user-library-read'
 redirect_url = 'http://localhost:8000/callback/'
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cid, client_secret=secret, scope=scope,
-                                               redirect_uri=redirect_url))
-
-# sp = SpotifyOAuth(client_id=cid, client_secret=secret, scope=scope, redirect_uri=redirect_url, show_dialog=True)
-# auth_url = sp.get_authorize_url()
-# print(f'{auth_url}')
-# url = input('paste: ')
-# code = sp.parse_auth_response_url(url)
+sp = SpotifyOAuth(client_id=cid, client_secret=secret, scope=scope, redirect_uri=redirect_url, open_browser=False, show_dialog=True)
 #
-# token = sp.get_access_token(code, as_dict=False, check_cache=False)
-# user = spotipy.Spotify(auth=token)
+# # Function to write tokens to a file
+# def write_tokens(tokens):
+#     with open(f"{sp.current_user()['display_name']}_tokens.json", "w") as f:
+#         json.dump(tokens, f)
+#
+#
+# # Function to read tokens from a file
+# def read_tokens():
+#     with open(f"{sp.current_user()['display_name']}_tokens.json", "r") as f:
+#         tokens = json.load(f)
+#     return tokens
+#
+# # Check if tokens are stored in file
+# try:
+#     tokens = read_tokens()
+#     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cid, client_secret=secret,
+#                                                    scope=scope, redirect_uri=redirect_url,
+#                                                    access_token=tokens["access_token"],
+#                                                    refresh_token=tokens["refresh_token"]))
+# except:
+#     # auth_url = sp.get_authorize_url()
+#     # print(f'{auth_url}')
+#     # url = input('paste: ')
+#     # code = sp.parse_auth_response_url(url)
+access_token = sp.get_access_token(check_cache=False, as_dict=False)
+# access_token = tokens['access_token']
+# refresh_token = tokens['refresh_token']
+sp = spotipy.Spotify(auth=access_token)
+
+#     # Store tokens in a dictionary and write to file
+#     tokens = {
+#         "access_token": access_token,
+#         "refresh_token": refresh_token
+#     }
+#     write_tokens(tokens)
+# finally:
+#     if isinstance(sp._session, requests.Session):
+#         sp._session.close()
+
+name = sp.current_user()['display_name']
 
 tracks = []
 for i in range(0, 5000, 50):
@@ -41,12 +74,6 @@ for track in tracks:
 
 sorted_artists = dict(sorted(artist_counts.items(), key=lambda x: x[1])[::-1])
 
-# artist_counts['Others'] = 0
-# for count in artist_counts.values():
-#     if count == 1:
-#         artist_counts['Others'] += count
-
-
 # Print the number of tracks by each artist
 for artist, count in sorted_artists.items():
     print(f"{artist}: {count}")
@@ -56,7 +83,5 @@ fig, ax = plt.subplots()
 ax.barh(artists_names[:10], tracks_count[:10])
 ax.set_xlabel('Artists')
 ax.set_ylabel('Number Of Songs')
-ax.set_title('Top 10 Artists by Saved Tracks')
+ax.set_title(f'Top 10 Artists by Saved Tracks for {name}')
 plt.show()
-
-
