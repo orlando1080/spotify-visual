60506bd5815002648781c8186b4620bb6e215515
orlando1080 <lewislsewell@gmail.com>
2023-04-15 17:51:26 +0100

    Cleaned up got and removed some commented out code

--- spotify_visual.py
+++ spotify_visual.py
@@ -1,7 +1,5 @@
 from collections import defaultdict
 import configparser
-import json
-import requests
 
 import matplotlib.pyplot as plt
 
@@ -16,45 +14,9 @@ secret = config.get('SPOTIFY', 'CLIENT_SECRET')
 scope = 'user-library-read'
 redirect_url = 'http://localhost:8000/callback/'
 sp = SpotifyOAuth(client_id=cid, client_secret=secret, scope=scope, redirect_uri=redirect_url, open_browser=False, show_dialog=True)
-#
-# # Function to write tokens to a file
-# def write_tokens(tokens):
-#     with open(f"{sp.current_user()['display_name']}_tokens.json", "w") as f:
-#         json.dump(tokens, f)
-#
-#
-# # Function to read tokens from a file
-# def read_tokens():
-#     with open(f"{sp.current_user()['display_name']}_tokens.json", "r") as f:
-#         tokens = json.load(f)
-#     return tokens
-#
-# # Check if tokens are stored in file
-# try:
-#     tokens = read_tokens()
-#     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cid, client_secret=secret,
-#                                                    scope=scope, redirect_uri=redirect_url,
-#                                                    access_token=tokens["access_token"],
-#                                                    refresh_token=tokens["refresh_token"]))
-# except:
-#     # auth_url = sp.get_authorize_url()
-#     # print(f'{auth_url}')
-#     # url = input('paste: ')
-#     # code = sp.parse_auth_response_url(url)
-access_token = sp.get_access_token(check_cache=False, as_dict=False)
-# access_token = tokens['access_token']
-# refresh_token = tokens['refresh_token']
-sp = spotipy.Spotify(auth=access_token)
 
-#     # Store tokens in a dictionary and write to file
-#     tokens = {
-#         "access_token": access_token,
-#         "refresh_token": refresh_token
-#     }
-#     write_tokens(tokens)
-# finally:
-#     if isinstance(sp._session, requests.Session):
-#         sp._session.close()
+access_token = sp.get_access_token(check_cache=False, as_dict=False)
+sp = spotipy.Spotify(auth=access_token)
 
 name = sp.current_user()['display_name']
 
@@ -74,9 +36,6 @@ for track in tracks:
 
 sorted_artists = dict(sorted(artist_counts.items(), key=lambda x: x[1])[::-1])
 
-# Print the number of tracks by each artist
-for artist, count in sorted_artists.items():
-    print(f"{artist}: {count}")
 artists_names = [name for name in sorted_artists]
 tracks_count = [num for num in sorted_artists.values()]
 fig, ax = plt.subplots()
